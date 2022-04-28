93e1429a7a91ace37e2e277d795d373f3a4eef97
apazze <apazze@users.noreply.github.com>
2022-04-28 15:20:26 -0300

    Adicionando usos de method reference ( :: ) e entendimento de default methods que possibilita a evolução das interfaces, possuem corpo. forEach() é um default method do Iterable.

--- src/br/com/alura/java8/OrdenaStrings.java
+++ src/br/com/alura/java8/OrdenaStrings.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 class OrdenaStrings {
 
@@ -14,20 +15,34 @@ class OrdenaStrings {
         palavras.add("editora casa do codigo");
         palavras.add("caelum");
 
-
+        System.out.println("Iterado com .forEach() do java 8:");
         palavras.forEach(p -> System.out.println(p));
-        //Mesma coisa que:
+        /*//Mesma coisa que:
         Consumer<String> impressor = p -> System.out.println(p);
         palavras.forEach(impressor);
-
+*/
 
         //palavras.sort((p1, p2) -> Integer.compare(p1.length(), p2.length()));
+        //Ou ainda:
+        //palavras.sort(Comparator.comparing(p -> p.length()));
+
+        //Quebrando o que foi feito acima:
+        /*Function<String, Integer> funcao = p -> p.length();
+        Comparator<String> comparador = Comparator.comparing(funcao);
+        palavras.sort(comparador);*/
+
+
+        //method reference ( :: )
+        // String::length = Dada uma string, invoca o length da mesma. Equivalente à p -> p.length()
+        // System.out::println
+        // Usado para quando é invocado apenas um método, ela será convertida para uma expressão lambda
+
         palavras.sort(Comparator.comparingInt(String::length));
 
-
+        System.out.println("Após ordenar com sort pela qtd de cars:");
         palavras.forEach(p -> System.out.println(p));
 
-        new Thread(new Runnable() {
+        /*new Thread(new Runnable() {
 
             @Override
             public void run() {
@@ -36,7 +51,7 @@ class OrdenaStrings {
 
         }).start();
 
-        new Thread(()-> System.out.println("Bla")).start();
+        new Thread(()-> System.out.println("Bla")).start();*/
 
     }
 }
